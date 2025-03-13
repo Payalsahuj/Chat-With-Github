471a658118b3e26966fbeb036cf291db1333e1f1
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-03-13 09:19:01 +0530

    Get embeddings for given text

--- app/services/documentServices.ts
+++ app/services/documentServices.ts
@@ -24,3 +24,27 @@ export const GetRepoInfo = async (owner: string, repo: string) => {
   }
   return { isNew, repo: response?.data?.[0] };
 };
+
+export const TextToEmbedding = async (text: string) => {
+  const url = "https://api.openai.com/v1/embeddings";
+  const headers = {
+    Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+    "Content-Type": "application/json",
+  };
+  const data = {
+    input: JSON.stringify(text),
+    model: "text-embedding-3-small",
+    encoding_format: "float",
+  };
+  return axios
+    .post(url, data, { headers })
+    .then((response) => {
+      return response.data?.data?.[0]?.embedding;
+    })
+    .catch((error) => {
+      console.error(
+        "Error:",
+        error.response ? error.response.data : error.message
+      );
+    });
+};
